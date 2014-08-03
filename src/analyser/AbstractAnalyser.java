a15c33f2d9d3abbfdd3066568342071dab80d358
Luke <sharlock@gmail.com>
2014-08-03 22:32:50 +1000

    public

--- src/analyser/AbstractAnalyser.java
+++ src/analyser/AbstractAnalyser.java
@@ -5,9 +5,9 @@ package analyser;
  */
 public abstract class AbstractAnalyser
 {
-    private int win = 0;
-    private int winNum = 0;
-    private int total = 0;
+    public int win = 0;
+    public int winNum = 0;
+    public int total = 0;
 
     public void accumulateWinPrice(int division)
     {
@@ -22,35 +22,5 @@ public abstract class AbstractAnalyser
     public void increaseTotalNum()
     {
         total++;
-    }
-
-    public int getWin()
-    {
-        return win;
-    }
-
-    public void setWin(int win)
-    {
-        this.win = win;
-    }
-
-    public int getWinNum()
-    {
-        return winNum;
-    }
-
-    public void setWinNum(int winNum)
-    {
-        this.winNum = winNum;
-    }
-
-    public int getTotal()
-    {
-        return total;
-    }
-
-    public void setTotal(int total)
-    {
-        this.total = total;
     }
 }
