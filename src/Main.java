c3cb6e70867dc8fdfaaf00c157e486fd9d295a19
luke <sharlock@gmail.com>
2014-08-06 09:37:25 +1000

    enable oz and powerball

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main
     public static void main(String[] args)
     {
         analyseOZ();
-        //analysePowerBall();
+        analysePowerBall();
     }
 
     private static void analyseOZ()
